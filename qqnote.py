7c6604fe8fe6f5d9cb2354feed93b75be575d524
cztchoice <cztchoice@gmail.com>
2012-08-31 15:23:02 +0800

    initial commit

---
+++ qqnote.py
@@ -0,0 +1,68 @@
+import HTMLParser
+import codecs
+
+
+class LinksParser(HTMLParser.HTMLParser):
+    def __init__(self):
+        HTMLParser.HTMLParser.__init__(self)
+        self.recording = 0
+        self.istitle = 0
+        self.istime = 0
+        self.iscontent = 0
+        self.data = []
+        self.title = []
+        self.time = []
+        self.content = []
+
+    def handle_starttag(self, tag, attributes):
+        if tag != 'div':
+            return
+        if self.recording:
+            self.recording += 1
+            return
+        for name, value in attributes:
+            if name == 'class' and value == 'qqshowbd':
+                break
+            elif name == 'class' and value == 'notetitle bigfont':
+                self.istitle = 1
+                print 'match it'
+                break
+            elif name == 'class' and value == 'graytext timesep':
+                self.istime = 1
+                print 'match it'
+                break
+            elif name == 'id' and value == 'content':
+                self.iscontent = 1
+                print 'match it'
+                break
+
+        else:
+            return
+        self.recording = 1
+
+    def handle_endtag(self, tag):
+        if tag == 'div' and self.recording:
+            self.recording -= 1
+            #self.istitle = 0
+            #self.istime = 0
+            #self.iscontent = 0
+
+    def handle_data(self, data):
+        if self.recording:
+            self.data.append(data)
+        if self.istitle:
+            self.title.append(data)
+        if self.istime:
+            self.time.append(data)
+        if self.iscontent:
+            self.content.append(data)
+
+parser = LinksParser()
+f = codecs.open('example.html', 'r', 'utf-8')
+html = f.read()
+parser.feed(html)
+for s in parser.data:
+    if s.strip() != '':
+        print s
+
+parser.close()
